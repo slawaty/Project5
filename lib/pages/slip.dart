51f726f37f3a1390bfafedaac518e8a836b57d66
slawaty <slawat000@gmail.com>
2020-11-09 13:12:46 +0700

    first commit

---
+++ lib/pages/slip.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:kirana/widgets/drawer.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+// class ShowSlip extends StatelessWidget {
+//   @override
+//   _ShowSlipState createState() => _ShowSlipState();
+
+// class
